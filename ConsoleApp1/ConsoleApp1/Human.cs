964e6d361116e55a8197e6df22701849d3d49b80
VINKOS <46785412+VINKOS7@users.noreply.github.com>
2026-07-24 03:20:18 +0700

    garbage delete

--- ConsoleApp1/ConsoleApp1/Human.cs
+++
@@ -1,6 +0,0 @@
-﻿internal class Human
-{
-    public Human(string birthDay)
-    {
-    }
-}
--- ConsoleApp1/ConsoleApp1/Program.cs
+++
@@ -1,3 +0,0 @@
-﻿Console.WriteLine("С Днем Рождения!");
-
-var Vadim = new Human(birthDay: "1995/03/02");
