711cab7fda703a9e1977772e3d16e98c6d1ebeeb
VINKOS <46785412+VINKOS7@users.noreply.github.com>
2026-07-26 15:03:28 +0700

    фигня

--- VQueries/VQueries/Controllers/Translator/TranslatorController.cs
+++ VQueries/VQueries/Controllers/Translator/TranslatorController.cs
@@ -17,29 +17,18 @@ public class TranslatorController : Controller
 
     public TranslatorController(IMediator mediator) => _mediator = mediator;
 
-    // Единственная ручка на модель-биндинге, и это осознанно: алфавит приезжает PERCENT-ENCODED,
-    // биндер его декодирует сам. Сырым слать нельзя ни при каких условиях - в алфавите есть '#',
-    // он начинает фрагмент, и всё после него до сервера просто не доедет.
     [AllowAnonymous] [HttpGet("/init")] public Task<InitResponse> Init(InitRequest request) => _mediator.Send(request);
 
-    // Руны едут в QUERY, а не в пути: в пути слэш разрывает сегмент и роут не матчится,
-    // а в query / и ? легальны. Читаем СЫРУЮ строку и не разбираем её на пары ключ-значение,
-    // иначе & и = из wire-алфавита развалят разбор.
     [AllowAnonymous] [HttpGet("/n")] public Task<TailResponse> Tail() => _mediator.Send(new TailRequest { Value = RawInt() });
 
     [AllowAnonymous] [HttpGet("/c")] public Task<CombineResponse> Combine() => _mediator.Send(new CombineRequest { Runes = RawArg() });
 
-    // hyper: известная ссылка одним запросом вместо всей цепочки /n + куски
     [AllowAnonymous] [HttpGet("/h")] public Task<HyperResponse> Hyper() => _mediator.Send(new HyperRequest { Value = RawInt() });
 
-    // всё после первого '=' - это и есть аргумент, каким бы он ни был
-    private string RawArg()
-    {
-        string raw = Request.QueryString.Value ?? "";
-        int eq = raw.IndexOf('=');
 
-        return eq < 0 ? "" : raw.Substring(eq + 1);
-    }
+    //after sometime will resolve this garbage
+    private string RawArg() => 
+        (Request.QueryString.Value ?? string.Empty).IndexOf('=') < 0 ? "" : Request.QueryString.Value ?? string.Empty.Substring((Request.QueryString.Value ?? string.Empty).IndexOf('=') + 1);
 
     private int RawInt() => int.TryParse(RawArg(), out int v) ? v : -1;
 }
